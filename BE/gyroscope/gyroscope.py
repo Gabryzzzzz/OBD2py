050e96d17af1f37b398c7d352e0444aeb65d17b7
Gabryzzzzz <gabriele.monti24@gmail.com>
2025-08-08 14:08:58 +0200

    forgotted var

--- BE/gyroscope/gyroscope.py
+++ BE/gyroscope/gyroscope.py
@@ -21,7 +21,7 @@ def get_info():
     return accelerazione_ext, giroscopio_ext, temperatura_ext
 
 def start_gyro():
-    global temperatura_ext, accelerazione_ext, giroscopio_ext, gyro_threshold
+    global temperatura_ext, accelerazione_ext, giroscopio_ext, gyro_threshold, last_time
     while True:
          # Calcola delta time
         now = time.time()
