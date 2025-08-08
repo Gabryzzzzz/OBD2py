c7ad6a5f9a1e314d215723de82e9daa3f241957b
Gabryzzzzz <gabriele.monti24@gmail.com>
2025-08-08 15:09:43 +0200

    eventlet updatted fix

--- BE/main.py
+++ BE/main.py
@@ -298,16 +298,18 @@ def stop_obd(sid):
 
     send_success('OBD Stop', 'OBD fermato con successo!')
 
+
+eventlet_pos = None
 # Avvia il server
 if __name__ == '__main__':
-    global eventlet_obd
-    eventlet.spawn(setup_display)
-    time.sleep(2)
+    global eventlet_obd, eventlet_pos
+    # time.sleep(2)
     print("🚀 Server WebSocket in esecuzione su porta 5000...")
     print("🚀 Server WebSocket in esecuzione")
     print("Inizio configurazione OBD...")
     # eventlet.spawn(configure_obd) must add a way to identify and kill it
     eventlet_obd = eventlet.spawn(configure_obd)
+    eventlet_pos = eventlet.spawn(setup_display)
     #get the locale ip with get_ip() and save it to a file into ../FE/src/assets/ip.txt
     ip = get_ip()
     print("📤 IP locale:", ip)
