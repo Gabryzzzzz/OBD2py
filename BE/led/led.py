30b806585105d56f18f064c576cad2fc8e206425
Gabryzzzzz <gabriele.monti24@gmail.com>
2025-08-08 15:37:37 +0200

    hajgbdwda

--- BE/led/led.py
+++ BE/led/led.py
@@ -7,8 +7,11 @@ def aggiungi_display(clk, dio):
 
 def setup_led_display():
     aggiungi_display(23, 24)
+    TMs[0].number(1234)
     aggiungi_display(25, 8)
+    TMs[1].number(1234)
     aggiungi_display(7, 1)
+    TMs[2].number(1234)
 
 
 
