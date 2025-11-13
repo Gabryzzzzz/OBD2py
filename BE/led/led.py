8c9d1453979352beac7667b5bb4ae947b5a058d8
Gabryzzzzz <gabriele.monti24@gmail.com>
2025-11-13 19:08:33 +0100

    fix alone

--- BE/led/led.py
+++ BE/led/led.py
@@ -7,11 +7,11 @@ def aggiungi_display(clk, dio):
 
 def setup_led_display():
     aggiungi_display(23, 24)
-    TMs[0].number(1234)
+    TMs[0].number(0000)
     aggiungi_display(25, 8)
-    TMs[1].number(1234)
+    TMs[1].number(0000)
     aggiungi_display(7, 1)
-    TMs[2].number(1234)
+    TMs[2].number(0000)
 
 
 
--- BE/main.py
+++ BE/main.py
@@ -162,7 +162,8 @@ def setup_display():
                 eventlet.spawn(led.TMs[1].numbers, int(y1), int(y2))
                 eventlet.spawn(led.TMs[2].numbers, int(z1), int(z2))
             if data_requested_led == "temp":
-                eventlet.spawn(led.TMs[1].temperature, int(temp))
+                # eventlet.spawn(led.TMs[1].temperature, int(temp))
+                pass
             time.sleep(0.3)
 
 
