5134721c1fa16ade073eaff9f6bdde225a58c264
Gabryzzzzz <gabriele.monti24@gmail.com>
2025-11-15 15:53:17 +0100

    awdwda

--- BE/main.py
+++ BE/main.py
@@ -8,7 +8,6 @@ from OBD_Handler import motore_prestazioni, altri_dati, consumi_carburante, temp
 from gyroscope import gyroscope
 from led import led
 from inputs import get_gamepad
-from ps3_controller import controller_ps3_main
 import os
 
 
@@ -315,59 +314,59 @@ def controller_ps3():
     # To store the state of the buttons (0=released, 1=pressed)
     button_states = {}
     print("Controller active. Press a button or move the stick. (Ctrl+C to exit)")
-    # while True:
-    #     # Get all available events from the gamepad
-    #     try:
-    #         events = get_gamepad()
-    #     except Exception:
-    #         print("Gamepad not found. Please connect a gamepad.")
-    #         break
+    while True:
+        # Get all available events from the gamepad
+        try:
+            events = get_gamepad()
+        except Exception:
+            print("Gamepad not found. Please connect a gamepad.")
+            break
 
-    #     for event in events:
-    #         # Update the stick state when an 'Absolute' event is received
-    #         if event.ev_type == 'Absolute':
-    #             if event.code == 'ABS_X':
-    #                 left_stick_x = event.state
-    #             elif event.code == 'ABS_Y':
-    #                 left_stick_y = event.state
+        for event in events:
+            # Update the stick state when an 'Absolute' event is received
+            if event.ev_type == 'Absolute':
+                if event.code == 'ABS_X':
+                    left_stick_x = event.state
+                elif event.code == 'ABS_Y':
+                    left_stick_y = event.state
 
-    #             # Get previous state for D-pad and Triggers
-    #             prev_state = button_states.get(event.code, 0)
+                # Get previous state for D-pad and Triggers
+                prev_state = button_states.get(event.code, 0)
 
-    #             # D-PAD: Check for transition from 0 (released) to not 0 (pressed)
-    #             if event.code in ('ABS_HAT0X', 'ABS_HAT0Y'):
-    #                 if event.state != 0 and prev_state == 0:
-    #                     print(f"D-Pad {event.code} pressed with value: {event.state}")
+                # D-PAD: Check for transition from 0 (released) to not 0 (pressed)
+                if event.code in ('ABS_HAT0X', 'ABS_HAT0Y'):
+                    if event.state != 0 and prev_state == 0:
+                        print(f"D-Pad {event.code} pressed with value: {event.state}")
 
-    #             # TRIGGERS: Check for transition from 0 to a pressed state
-    #             if event.code in ('ABS_Z', 'ABS_RZ'): # Corresponds to LT/L2 and RT/R2
-    #                 if event.state > 0 and prev_state == 0:
-    #                     print(f"Trigger {event.code} was pressed")
+                # TRIGGERS: Check for transition from 0 to a pressed state
+                if event.code in ('ABS_Z', 'ABS_RZ'): # Corresponds to LT/L2 and RT/R2
+                    if event.state > 0 and prev_state == 0:
+                        print(f"Trigger {event.code} was pressed")
 
-    #             button_states[event.code] = event.state
-    #         # Handle button presses
-    #         elif event.ev_type == 'Key':
-    #             # Get the previous state of the button, defaulting to 0 (released)
-    #             prev_state = button_states.get(event.code, 0)
-    #             # Check if the button is being pressed now and was released before
-    #             if event.state == 1 and prev_state == 0:
-    #                 if event.code == 'BTN_START':
-    #                     print("Start button pressed")
-    #                 elif event.code == 'BTN_SELECT':
-    #                     print("Select button pressed")
-    #                 else:
-    #                     if event.code == 'BTN_SOUTH':
-    #                         print("X button pressed, cycling LED config")
-    #                         # if data_requested_led == "acc":
-    #                         #     data_requested_led = "gyr"
-    #                         # elif data_requested_led == "gyr":
-    #                         #     data_requested_led = "temp"
-    #                         # elif data_requested_led == "temp":
-    #                         #     data_requested_led = "acc"
-    #                         # send_success('LED Config', f'LED display now showing: {data_requested_led}')
+                button_states[event.code] = event.state
+            # Handle button presses
+            elif event.ev_type == 'Key':
+                # Get the previous state of the button, defaulting to 0 (released)
+                prev_state = button_states.get(event.code, 0)
+                # Check if the button is being pressed now and was released before
+                if event.state == 1 and prev_state == 0:
+                    if event.code == 'BTN_START':
+                        print("Start button pressed")
+                    elif event.code == 'BTN_SELECT':
+                        print("Select button pressed")
+                    else:
+                        if event.code == 'BTN_SOUTH':
+                            print("X button pressed, cycling LED config")
+                            # if data_requested_led == "acc":
+                            #     data_requested_led = "gyr"
+                            # elif data_requested_led == "gyr":
+                            #     data_requested_led = "temp"
+                            # elif data_requested_led == "temp":
+                            #     data_requested_led = "acc"
+                            # send_success('LED Config', f'LED display now showing: {data_requested_led}')
 
-    #                     print(f"Button {event.code} was pressed")
-    #             button_states[event.code] = event.state
+                        print(f"Button {event.code} was pressed")
+                button_states[event.code] = event.state
 
 # Avvia il server
 if __name__ == '__main__':
@@ -376,7 +375,8 @@ if __name__ == '__main__':
     eventlet.spawn(gyroscope.start_gyro)
     time.sleep(2)
     eventlet.spawn(setup_display)
-
+    print("🎮 Avvio controller PS3")
+    eventlet.spawn(controller_ps3)
     print("🚀 Server WebSocket in esecuzione su porta 5000...")
     print("🚀 Server WebSocket in esecuzione")
     print("Inizio configurazione OBD...")
@@ -395,6 +395,5 @@ if __name__ == '__main__':
     template = template.replace('PLACEHOLDER', ip)
     with open('../FE/src/assets/ip.ts', 'w') as f:
         f.write(template)
-    print("🎮 Avvio controller PS3")
-    eventlet.spawn(controller_ps3_main.controller_ps3)
+
     eventlet.wsgi.server(eventlet.listen(('0.0.0.0', 5000)), app)
